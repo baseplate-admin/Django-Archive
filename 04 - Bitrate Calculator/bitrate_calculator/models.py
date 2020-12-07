98d16491c5d22f7e86babd15deb7b690e498245c
baseplate-admin <61817579+baseplate-admin@users.noreply.github.com>
2020-12-07 18:07:31 +0600

    Reformated Form to make it look better

--- 04 - Bitrate Calculator/bitrate_calculator/models.py
+++ 04 - Bitrate Calculator/bitrate_calculator/models.py
@@ -18,10 +18,10 @@ class Bitrate(models.Model):
 
 
 class ModelBitrate(forms.Form):
-    hour = forms.IntegerField(label="Hour:")
-    minute = forms.IntegerField(label="Minute:")
-    second = forms.IntegerField(label="Second:")
+    hour = forms.IntegerField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    minute = forms.IntegerField(widget=forms.TextInput(attrs={'class': 'form-control'}))
+    second = forms.IntegerField(widget=forms.TextInput(attrs={'class': 'form-control'}))
     size = forms.DecimalField(
-        label="Preferred Size in GB(Upto 2 decimal value allowed)", decimal_places=2
+        decimal_places=2, widget=forms.TextInput(attrs={'class': 'form-control'})
     )
-    episode = forms.IntegerField(label="No of Episodes:")
+    episode = forms.IntegerField(widget=forms.TextInput(attrs={'class': 'form-control'}))
